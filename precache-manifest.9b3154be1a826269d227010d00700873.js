d1a566237da43e61199c6d83bca572fed0e0f888
Wagner <wagner@MacBook-Pro-de-Wagner.local>
2020-05-24 19:51:02 -0300

    Updates

---
+++ precache-manifest.9b3154be1a826269d227010d00700873.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53f49fa49f85775dc8144350d269ca1",
+    "url": "/Order-FO4E35082A04/index.html"
+  },
+  {
+    "revision": "232d0e61474155cbc393",
+    "url": "/Order-FO4E35082A04/static/js/2.b5712346.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/Order-FO4E35082A04/static/js/2.b5712346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc2cd21c107d27073b0",
+    "url": "/Order-FO4E35082A04/static/js/main.4879bb1c.chunk.js"
+  },
+  {
+    "revision": "099b907cccc68153efde",
+    "url": "/Order-FO4E35082A04/static/js/runtime-main.15aab057.js"
+  }
+]);
